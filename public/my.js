a4a0527e8a898880e012d7ddaed0061e05236a6c
jagerkristof <jagerkristof@ugmszi.hu>
2023-01-27 12:08:30 +0100

    javítás

--- public/my.js
+++ public/my.js
@@ -1,1 +1,29 @@
 console.log("frontend: heló");
+const url = "http://localhost:3000/products";
+
+async function getProducts(){
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+}
+
+async function postProduct(){
+    let body = {
+            name: "Á9",
+            quantity: 66,
+            price: 6500,
+            type: "fémáru"
+    }
+
+    let response = await fetch(url, {
+        Method: 'POST',
+        Headers: {
+          Accept: 'application.json',
+          'Content-Type': 'application/json'
+        },
+        Body: body,
+        Cache: 'default'
+      })
+}
+
+getProducts();
